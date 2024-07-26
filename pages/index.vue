804dfe3df9cf1bffc6425e90b39d201060fd27f4
maulanairfanf <maulanairfanf@gmail.com>
2024-07-26 08:32:57 +0700

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,60 @@
 <template>
 	<div>
 		<ul>
-			<li v-for="cafe in cafeStore.cafes.data" :key="cafe.id">
-				{{ cafe.company.name }}
-			</li>
+			<li
+				v-for="cafe in data"
+				:key="cafe.id"
+				v-text="cafe.company.name + ' - ' + cafe.location_name"
+			></li>
 		</ul>
-
-		<button
-			@click="cafeStore.loadMore()"
-			v-if="cafeStore.hasMore"
-			:disabled="status === 'pending'"
-		>
-			Load More
-		</button>
+		<!-- <button @click="previous()" v-if="pagination.page > 1">Previous</button>
+		<button @click="next()" v-if="pagination.page < cafes.last_page">
+			Next
+		</button> -->
+		<button @click="previous()" v-if="page > 1">Previous</button>
+		<button @click="next()" v-if="page < cafes.last_page">Next</button>
 	</div>
 </template>
-
 <script setup>
-import { useCafeStore } from '../stores/cafeStore'
+const pagination = reactive({
+	page: 1,
+})
 
-const cafeStore = useCafeStore()
+const page = ref(1)
+const data = ref([])
+const lastPage = ref(1)
 
-const { data, status } = await useAsyncData('cafes', () =>
-	cafeStore.fetchCafes()
+const url = computed(
+	() => `https://api.roastandbrew.coffee/api/v1/cafes?page=${page.value}`
 )
 
-cafeStore.cafes.data = data.value.data
-cafeStore.cafes.last_page = data.value.last_page
+const { data: cafes, error } = await useFetch(url)
+
+data.value = cafes.value.data
+lastPage.value = cafes.value.last_page
+
+const previous = () => {
+	if (page.value != 1) {
+		page.value -= 1
+	}
+}
+
+const next = () => {
+	if (page.value + 1 <= cafes.value.last_page) {
+		page.value += 1
+		console.log(('page', page.value))
+	}
+}
+
+watch(
+	() => cafes.value,
+	newVal => {
+		if (newVal) {
+			console.log('newVal', newVal)
+			data.value = newVal.data
+			lastPage.value = newVal.last_page
+			console.log('data.value', data.value)
+		}
+	}
+)
 </script>
